7edb1b78c8d6f41110334b58e42cde53775b2a0c
Daffalih <daffalih75@gmail.com>
2026-05-04 21:13:30 +0700

    ABP

---
+++ Pertemuan 8/praktikdart/variable.dart
@@ -0,0 +1,11 @@
+//var
+var variableName;
+var name = "Daffa";
+
+//type annotation
+String? nama;
+String namaLengkap = "Daffa Falih Aqilah";
+int? a, b, c;
+
+//multiple variable
+int x = 1, y = 2, z = 3;
